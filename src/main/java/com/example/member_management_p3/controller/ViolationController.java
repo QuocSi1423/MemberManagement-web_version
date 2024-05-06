44785d808ace5e4c571ac5b2a3d94f5250962c2c
Calcifer <ainhan610@gmail.com>
2024-05-06 22:22:42 +0700

    update: show detail page
    add new
    show detail

--- src/main/java/com/example/member_management_p3/controller/ViolationController.java
+++ src/main/java/com/example/member_management_p3/controller/ViolationController.java
@@ -7,13 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.member_management_p3.Global;
+import com.example.member_management_p3.model.SimpleMember;
 import com.example.member_management_p3.model.Violation;
 import com.example.member_management_p3.service.ViolationService;
 
 @Controller
+@RequestMapping("/admin/violations")
 public class ViolationController {
 
     @Autowired
@@ -24,7 +30,7 @@ public class ViolationController {
     }
 
     
-    @GetMapping("/admin/violations")
+    @GetMapping
     public String showViolationPage(@RequestParam(value = "filter", required = false) String filter, Model model) {
         Integer status = null;
         Date startDate = null;
@@ -54,75 +60,31 @@ public class ViolationController {
         return "admin/violation/violation";
     }
 
-    // @GetMapping("/admin/member/get_all_members")
-    // public String getAllMembers(Model model) {
-    //     System.out.println("Get all members");
-    //     ResponseStructure<List<Member>> response = service.getAllMembers();
-    //     model.addAttribute("response", response);
-    //     return "getAllMembers";
-    // }
+    @GetMapping("/{id}")
+    public String showDetailViolationPage(@PathVariable Integer id, Model model) {
+        Violation violation;
+        violation = violationService.getViolation(id);
+        
+        model.addAttribute("violation", violation);
 
-    // @GetMapping("/admin/member/get_member/{id}")
-    // public String getMember(@PathVariable Long id, Model model) {
-    //     System.out.println("Get member by id");
-    //     ResponseStructure<Member> response = service.getMemberById(id);
-    //     model.addAttribute("response", response);
-    //     return "getMember";
-    // }
+        return "admin/violation/violation-detail";
+    }
 
-    // @GetMapping("/admin/member/get_members_by_name/{name}")
-    // public String getMembersByName(@PathVariable String name, Model model) {
-    //     System.out.println("Get members by name");
-    //     ResponseStructure<List<Member>> response = service.getMembersByName(name);
-    //     model.addAttribute("response", response);
-    //     return "getMembersByName";
-    // }
+    @GetMapping("/new")
+    public String showEmptyDetailViolationPage(Model model) {
+        Violation violation = new Violation();
+        violation.setMember(new SimpleMember());
+        
+        model.addAttribute("violation", violation);
 
-    // @PostMapping("/admin/member/add_member")
-    // public String addMember(@RequestBody Member member, Model model) {
-    //     System.out.println("Add member");
-    //     ResponseStructure<Member> response = service.addMember(member);
-    //     model.addAttribute("response", response);
-    //     return "addMember";
-    // }
+        return "admin/violation/violation-detail";
+    }
 
-    // @PostMapping("/admin/member/add_multiple_members")
-    // public String addMultipleMembers(@RequestBody List<Member> members, Model model) {
-    //     System.out.println("Add multiple members");
-    //     ResponseStructure<Member> response = service.addMultipleMembers(members);
-    //     model.addAttribute("response", response);
-    //     return "addMultipleMembers";
-    // }
-
-    // @PutMapping("/admin/member/update_member")
-    // public String updateMember(@RequestBody Member member, Model model) {
-    //     System.out.println("Update member");
-    //     ResponseStructure<Member> response = service.updateMember(member);
-    //     model.addAttribute("response", response);
-    //     return "updateMember";
-    // }
-
-    // @DeleteMapping("/admin/member/delete_member/{id}")
-    // public String deleteMember(@PathVariable Long id, Model model) {
-    //     System.out.println("Delete member");
-    //     ResponseStructure<Member> response = service.deleteMember(id);
-    //     model.addAttribute("response", response);
-    //     return "deleteMember";
-    // }
-
-    // @PostMapping("/admin/member/delete_members_by_conditions")
-    // public String deleteMembersByConditions(@RequestBody MemberDeleteCondition conditions, Model model) {
-    //     System.out.println("Delete members by conditions");
-    //     System.out.println(conditions.toString());
-    //     ResponseStructure<Member> response = service.deleteMembersByConditions(conditions.getKhoa(), conditions.getNganh(), conditions.getMaTVSubstring());
-    //     model.addAttribute("response", response);
-    //     return "deleteMembersByConditions";
-    // } 
-
-    // @PostMapping("/admin/add_members/submit_file")
-    // public String submitFile(@RequestParam("file") MultipartFile file, Model model) {
-    //     ResponseStructure<Member> response = service.addMembersFromFile(file);
-    //     model.addAttribute("response", response);
-    //     return "submitFile";
-    // }
+    @PostMapping("/new")
+    public String addViolation(@ModelAttribute Violation violation) {
+        if (violationService.insertViolation(violation) == Global.SUCCESSFUL)
+            return "redirect:/admin/violations";
+        else
+            return "redirect:/admin/violations/new";
+    }
 }
