f08d1928d27ef72c4ed4eff9ce58dc313df95b36
Ngan Ngo <ngolehuengan170203@gmail.com>
2024-05-08 10:36:36 +0700

    done UI

--- src/main/java/com/example/member_management_p3/Controller/ProfileController.java
+++ src/main/java/com/example/member_management_p3/Controller/ProfileController.java
@@ -1,4 +1,4 @@
-package com.example.member_management_p3.controller;
+package com.example.member_management_p3.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/example/member_management_p3/controller/ViolationController.java
+++ src/main/java/com/example/member_management_p3/controller/ViolationController.java
@@ -1,4 +1,4 @@
-package com.example.member_management_p3.controller;
+package com.example.member_management_p3.Controller;
 
 import java.util.Date;
 import java.util.List;
